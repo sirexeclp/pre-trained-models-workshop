aa6a5746a2019e09ceddefce314d2f88fe7fb6c3
Felix Grzelka <felix.grzelka@hpi.de>
2023-06-07 12:00:33 +0200

    fix transcribe_and_summarize

--- transcribe.py
+++ transcribe.py
@@ -1,58 +1,57 @@
+"""This module implements the transcribe and summarization logic."""
 import base64
 import os
-from celery import Celery, chain
-from fastapi import UploadFile
-import whisper
-from whisper.audio import SAMPLE_RATE
-from typing import Annotated
 
-from fastapi import FastAPI, File, Form, UploadFile
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-import numpy as np
 import ffmpeg
+import numpy as np
+import whisper
+from celery import Celery
 from transformers import pipeline
+from whisper.audio import SAMPLE_RATE
+
+mq_host = os.getenv("MQ_HOST")
+
+app = Celery("tasks", backend=f"rpc://{mq_host}", broker=f"pyamqp://{mq_host}")
 
 
-app = Celery("tasks", backend="rpc://rabbitmq", broker="pyamqp://rabbitmq")
-
-
-def load_audio(file: str, sr: int = SAMPLE_RATE):
+def audio_from_base64(base64_str: str, sample_rate: int = SAMPLE_RATE):
+    """Load audio from a base64 string and return numpy array."""
     try:
         out, _ = (
             ffmpeg.input("pipe:", threads=0)
-            .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
+            .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sample_rate)
             .run(
                 cmd=["ffmpeg", "-nostdin"],
-                input=base64.b64decode(file),
+                input=base64.b64decode(base64_str),
                 capture_stdout=True,
                 capture_stderr=True,
             )
         )
-    except ffmpeg.Error as e:
-        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+    except ffmpeg.Error as error:
+        raise RuntimeError(f"Failed to load audio: {error.stderr.decode()}") from error
 
     return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
 
 
 @app.task
-def transcribe(audio_file: UploadFile, model_size: str):
+def transcribe(base64_audio: str, model_size: str):
+    """Return a transcription of the audio, generated with whisper."""
     model = whisper.load_model(
         model_size, download_root=os.environ.get("TRANSFORMERS_CACHE")
     )
-    audio = load_audio(audio_file)
-    result = model.transcribe(audio)
-    return result["text"]
-    # text = result["text"]
-    # print(text)
+    audio = audio_from_base64(base64_audio)
+    return model.transcribe(audio)["text"]
 
 
 @app.task
-def summarize(text: str, max_length=130, min_length=30):
-    summarizer = pipeline("summarization", model="pszemraj/long-t5-tglobal-base-16384-book-summary")
-    return summarizer(text)[0]["summary_text"]
+def summarize(text: str, max_length=100, min_length=30):
+    """Return a summary of the input text."""
+    summarizer = pipeline(
+        "summarization", model="pszemraj/long-t5-tglobal-base-16384-book-summary"
+    )
+    return summarizer(text, min_length=min_length, max_length=max_length)[0][
+        "summary_text"
+    ]
 
 
-@app.task
-def transcribe_and_summarize(audio_file, model_size: str):
-    return (transcribe.s(audio_file, model_size) | summarize.s())()
+transcribe_and_summarize = transcribe.s() | summarize.s()
