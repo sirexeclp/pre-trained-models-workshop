7496d07d4ff92c34c2d7d75601ab4696e9a49253
Felix Grzelka <felix.grzelka@hpi.de>
2023-06-07 12:00:59 +0200

    black, pylint & co.

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
+"""This is our fastapi backend."""
 import base64
-import whisper
-from whisper.audio import SAMPLE_RATE
 
-from fastapi import FastAPI, File, Form, UploadFile
+from fastapi import FastAPI, Form, UploadFile
 from fastapi.staticfiles import StaticFiles
 
-from transcribe import transcribe
+from transcribe import transcribe_and_summarize
 
 app = FastAPI()
 
 
 @app.post("/upload")
 async def create_upload_file(file: UploadFile, model_size: str = Form()):
+    """File upload handler."""
     with file.file as the_actual_file:
-        result = transcribe.delay(base64.b64encode(the_actual_file.read()).decode("UTF-8"), model_size)
-    return  {"text": result.wait()}
+        result = transcribe_and_summarize.delay(
+            base64.b64encode(the_actual_file.read()).decode("UTF-8"), model_size
+        )
+    return {"text": result.wait()}
 
 
+# serve static html, css and js files
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
